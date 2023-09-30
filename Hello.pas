4b4d3008e473df5e5ba08684c2d5fa5ddc26c5e0
Artem Filonov <afilono@sfedu.ru>
2023-09-30 10:26:01 +0300

    Переписал файл по заданию

--- Hello.pas
+++ Hello.pas
@@ -1,1 +1,13 @@
-﻿print('Hello, world!')
+﻿begin
+  var hour := ReadInteger('Введите час');
+  
+  Assert((hour >= 0) and (hour <= 23));
+  
+  case hour of
+    4..10: print('Доброе утро, мир!');
+    11..16: print('Добрый день, мир!');
+    17..22: print('Добрый вечер, мир!')
+  else print('Доброй ночи, мир!');
+  end;
+  
+end.
