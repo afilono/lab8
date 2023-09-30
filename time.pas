eb8cebf0095d8e3adbecc30eba317dd8df2188c6
Artem Filonov <afilono@sfedu.ru>
2023-09-30 10:39:39 +0300

    Задание 2, получается.

---
+++ time.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var year := ReadInteger('Введите год');
+  
+  if (year mod 100 = 0) and (year mod 400 <> 0) then
+    print('Не високосный')
+  else if (year mod 100 = 0) and (year mod 400 = 0) then
+    print('Високосный')
+  else if (year mod 4 = 0) and (year mod 100 <> 0) then
+    print('Високосный')
+  else
+    print('Не високосный');
+end.
